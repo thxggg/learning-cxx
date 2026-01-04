ba24bd0a4d05fea5dd2c3b372ed42aa4bd8da1e2
gg <12093483+ggcheerup@user.noreply.gitee.com>
2026-01-04 23:49:54 +0800

    update:11-20

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -7,21 +7,34 @@
 class DynFibonacci {
     size_t *cache;
     int cached;
+    int capacity;
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity) : cache(new size_t[capacity]), cached(1), capacity(capacity) {
+        cache[0] = 0;
+        cache[1] = 1;
+    }
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &) = delete;
+    DynFibonacci(DynFibonacci const &other)
+        : cache(new size_t[other.capacity]), cached(other.cached), capacity(other.capacity) {
+        for (int i = 0; i < capacity; ++i) {
+            cache[i] = other.cache[i];
+        }
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci()
+    {
+        delete[] cache;
+    };
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
-            cache[cached] = cache[cached - 1] + cache[cached - 2];
+        ASSERT(i >= 0 && i < capacity, "i out of range");
+        for (; cached < i; ++cached) { 
+            cache[cached + 1] = cache[cached] + cache[cached - 1];
         }
         return cache[i];
     }
